40e33b04ddc96b7f18255ca28f45e73df694290e
KR <kr@dc.ag>
2025-01-03 13:48:43 +0100

    changed structure

--- index.php
+++ index.php
@@ -1,18 +1,38 @@
-<?php
-// Dynamischer Titel für die Seite
-$title = 'Crafting Digital Worlds';
-
-// Header einbinden
-include '/structure/header.php';
-?>
-
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo $title ?? 'TLDR'; ?></title>
+    <link rel="stylesheet" href="/style/css/main.css">
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="headerLogo">
+                <img src="/content/img/Logo_KdR.svg" alt="Logo TLDR">
+            </div>
+            <nav class="headerNav">
+                <ul>
+                    <li><a href="#about">Über mich</a></li>
+                    <li><a href="#Skills">Skills</a></li>
+                    <li><a href="#works">Meine Arbeit</a></li>
+                    <li><a href="#contact">Kontakt</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+    </header>
+    <main>
 <section>
     <div class="hero">
-        
+
     </div>
 </section>
-
-<?php
-// Footer einbinden
-include '/structure/footer.php';
-?>
+</main>
+    <footer>
+        <p>this is the footer</p>
+    </footer>
+</body>
+</html>
